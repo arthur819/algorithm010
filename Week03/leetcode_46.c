22439828eb6df6f40d36ddbfd49b10f60d86fc27
Jiaming <905375751@qq.com>
2020-06-26 23:43:28 +0800

    improve code of leetcode 46 全排列
    Signed-off-by: Jiaming <905375751@qq.com>

--- Week03/leetcode_46.c
+++ Week03/leetcode_46.c
@@ -1,6 +1,6 @@
 #define MAX_LEN (20000)
-void recursion(int arr_len, int *arr, int numsSize, int **result, int *returnSize, int **returnColumnSizes, int *used, int *nums){
-    int *next_arr, *next_used;
+void recursion(int arr_len, int *arr, int numsSize, int **result, int *returnSize, int **returnColumnSizes, bool *used, int *nums){
+    int *next_arr;
     int i, j;
     if (arr_len == numsSize) {
         (*returnColumnSizes)[*returnSize] = arr_len;
@@ -8,27 +8,24 @@ void recursion(int arr_len, int *arr, int numsSize, int **result, int *returnSiz
         memcpy(result[*returnSize], arr, sizeof(int) * arr_len);
         ++(*returnSize);
         free(arr);
-        free(used);
         return;
     }
     for (i = 0; i < numsSize; ++i) {
-        if (used[i] == 1)
+        if (used[i] == true)
             continue;
-        next_used = (int *)calloc(numsSize, sizeof(int)); 
+	used[i] = true;
         next_arr = (int *)calloc(numsSize, sizeof(int));
-        memcpy(next_used, used, numsSize * sizeof(int));
-        next_used[i] = 1;
         memcpy(next_arr, arr, numsSize * sizeof(int));
         next_arr[arr_len] = nums[i];
-        recursion(arr_len + 1, next_arr, numsSize, result, returnSize, returnColumnSizes, next_used, nums);
+        recursion(arr_len + 1, next_arr, numsSize, result, returnSize, returnColumnSizes, used, nums);
+	used[i] = false;
     }
     free(arr);
-    free(used);
 }
 int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
     int **result = (int **)malloc(sizeof(int *) * MAX_LEN);
     int *arr = (int *)calloc(numsSize, sizeof(int));
-    int *used = (int *)calloc(numsSize, sizeof(int));
+    bool *used = (bool *)calloc(numsSize, sizeof(bool));
     *returnSize = 0;
     if (numsSize == 0) {
         *returnColumnSizes = NULL;
@@ -36,5 +33,6 @@ int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes)
     }
     (*returnColumnSizes) = (int *)calloc(MAX_LEN, sizeof(int));
     recursion(0, arr, numsSize, result, returnSize, returnColumnSizes, used, nums);
+    free(used);
     return result;
 }
